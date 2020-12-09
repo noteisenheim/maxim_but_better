ec136a59620be1ee805f5b4022c50d20121ca9c1
Chepurova Alla <43718473+screemix@users.noreply.github.com>
2020-12-09 23:19:44 +0300

    Add files via upload
    Improved hashed passwords

--- fill_db.py
+++ fill_db.py
@@ -14,7 +14,7 @@ def create_connection(db_file):
         if conn:
             cur = conn.cursor()
             cur.execute("CREATE TABLE credentials (login TEXT PRIMARY KEY, password TEXT NOT NULL)")
-            cur.execute("INSERT INTO credentials (login, password) VALUES('alla', '\xff\xf1G\xd2s\xbe\x19\xff\xbem\xc5|\xcb\xb4\xba\xf2\x88-\xed\x01'), ('amina', '\xc2>w\xc8\x1a\x15\xdb\x03\x1a\xef\xdc\xa8|\xe2h3@\xa2\xa5\xde')")
+            cur.execute("INSERT INTO credentials (login, password) VALUES('alla', '\\xff\\xf1G\\xd2s\\xbe\\x19\\xff\\xbem\\xc5|\\xcb\\xb4\\xba\\xf2\\x88-\\xed\\x01'), ('amina', '\\xc2>w\\xc8\\x1a\\x15\\xdb\\x03\\x1a\\xef\\xdc\\xa8|\\xe2h3@\\xa2\\xa5\\xde')")
             conn.commit()
             conn.close()
 
